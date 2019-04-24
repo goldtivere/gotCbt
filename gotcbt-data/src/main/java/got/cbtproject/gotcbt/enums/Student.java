120f04c47df760bc5491292bc4aec94555fff486
goldtivere <goldtive@gmail.com>
2019-04-24 18:13:50 +0100

    working on student display javascript

---
+++ gotcbt-data/src/main/java/got/cbtproject/gotcbt/enums/Student.java
@@ -0,0 +1,5 @@
+package got.cbtproject.gotcbt.enums;
+
+public enum Student {
+    UPLOAD,REGISTER,MANAGE_STUDENT
+}
--- gotcbt-web/src/main/java/got/cbtproject/gotcbt/controller/AdminController.java
+++ gotcbt-web/src/main/java/got/cbtproject/gotcbt/controller/AdminController.java
@@ -1,6 +1,8 @@
 package got.cbtproject.gotcbt.controller;
 
+import got.cbtproject.gotcbt.enums.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 
     @GetMapping("student")
-    public String student(){
+    public String student(Model model){
+        model.addAttribute("student", Student.values());
         return "admin/student";
     }
 
