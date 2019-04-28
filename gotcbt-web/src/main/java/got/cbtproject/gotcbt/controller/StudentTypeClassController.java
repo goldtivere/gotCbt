b5097c96f2a8d66ab3eb0426d2cf92339a88376a
goldtivere <goldtive@gmail.com>
2019-04-29 00:12:56 +0100

    saving school group

--- gotcbt-web/src/main/java/got/cbtproject/gotcbt/controller/StudentTypeClassController.java
+++ gotcbt-web/src/main/java/got/cbtproject/gotcbt/controller/StudentTypeClassController.java
@@ -2,12 +2,16 @@ package got.cbtproject.gotcbt.controller;
 
 import got.cbtproject.gotcbt.command.StudentClassCommand;
 import got.cbtproject.gotcbt.services.StudentClassTypeService;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@Getter
+@Setter
 public class StudentTypeClassController {
     private final StudentClassTypeService studentClassTypeService;
 
@@ -22,7 +26,11 @@ public class StudentTypeClassController {
         try {
             StudentClassCommand studentClassCommand1=studentClassTypeService.save(schoolClass);
             redirectAttributes.addFlashAttribute("msg", "success");
-        } catch (Exception e) {
+        }catch (RuntimeException e) {
+            redirectAttributes.addFlashAttribute("msg", "exist");
+        }
+        catch (Exception e) {
+//            e.printStackTrace();
             redirectAttributes.addFlashAttribute("msg", "fail");
 //            logger.error("save: " + e.getMessage());
         }
